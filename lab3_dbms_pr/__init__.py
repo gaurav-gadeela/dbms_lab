7c55245e796b35271f0efed6c7754c4d7c7df023
gaurav-gadeela <88039739+gaurav-gadeela@users.noreply.github.com>
2021-09-24 23:26:45 +0530

    Delete lab3_dbms_pr directory

--- lab3_dbms_pr/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask,request,Response
-
-app = Flask(__name__)
-
-from .views import *
--- lab3_dbms_pr/server.py
+++
@@ -1,9 +0,0 @@
-from flaskapp import app
-from flask_cors import CORS,cross_origin
-
-cors= CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host = "127.0.0.1",port=5000)

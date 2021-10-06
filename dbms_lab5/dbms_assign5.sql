9898c8cfeed05133827298904a151a4f07719755
gaurav-gadeela <88039739+gaurav-gadeela@users.noreply.github.com>
2021-10-06 23:57:13 +0530

    Delete dbms_lab5 directory

--- dbms_lab5/dbms_assign5.sql
+++
@@ -1,9 +0,0 @@
-select name from students where team='R' union select name from students where gender='F';.
-
-select team,count(name) as students from students group by team;
-
-select avg(math) ,team from students where gender='M' group by team;
-
-select * from students group by gender,team order by team;
-
-select avg(sci) ,team from students group by team order by avg(sci) desc  limit 2;
